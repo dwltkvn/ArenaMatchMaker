b674d724551e23978a12737169bb57678a31fa45
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2020-11-08 23:41:51 +0100

    add progress to abort btn during opponentFoundCmpt

--- src/components/opponentFoundComponent.js
+++ src/components/opponentFoundComponent.js
@@ -12,29 +12,42 @@ import Button from "@material-ui/core/Button"
 import Typography from "@material-ui/core/Typography"
 import TextField from "@material-ui/core/TextField"
 import InputAdornment from "@material-ui/core/InputAdornment"
+import Box from "@material-ui/core/Box"
+import LinearProgress from "@material-ui/core/LinearProgress"
 
 // material-ui icons import
 import IconButton from "@material-ui/core/IconButton"
 import FileCopyIcon from "@material-ui/icons/FileCopy"
 
-const styles = {}
+const styles = {
+  boxBorder: { border: "dashed red" }
+}
 
 class OpponentFoundComponent extends React.Component {
   constructor(props) {
     super(props)
     // bind function to this class
     //this.register = this.register.bind(this)
-
+    this.maxCount = 30
     // init the class state
-    this.state = {}
+    this.state = { stateCount: 0 }
 
     // classes variables
     //this.ts = 0
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      if (this.state.stateCount >= this.maxCount) this.props.cbOnAbort()
+      this.setState(prevState => {
+        return { stateCount: prevState.stateCount + 1 }
+      })
+    }, 1000)
+  }
 
-  componentWillUnmount() {}
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
   render() {
     const classes = styles
@@ -63,14 +76,21 @@ class OpponentFoundComponent extends React.Component {
         </Grid>
         <Grid item xs={12} />
         <Grid container justify="space-evenly">
-          <Button
-            color="secondary"
-            size="large"
-            variant="outlined"
-            onClick={() => props.cbOnAbort()}
-          >
-            Abort
-          </Button>
+          <Box>
+            <Button
+              color="secondary"
+              size="large"
+              variant="outlined"
+              onClick={() => props.cbOnAbort()}
+            >
+              Abort
+            </Button>
+            <LinearProgress
+              variant="determinate"
+              value={(this.state.stateCount * 100) / this.maxCount}
+              color="secondary"
+            />
+          </Box>
           <Button color="primary" size="large" variant="outlined">
             Confirm
           </Button>
